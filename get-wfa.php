21d0bb3238b9a48d8baf35f13a86e703d67736b7
Chris Saldanha <csaldanha@itagroup.com>
2019-01-17 08:23:23 -0600

    Added file

---
+++ get-wfa.php
@@ -0,0 +1,24 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+
+$service_url = 'https://api.webflow.com/collections/5c1a5c3b6edb847166d0bcc8/items?api_version=1.0.0&access_token=a6bb5ed5a1d1a88a0ef1a37be52f58c12892c489981aff5f8db49d8a37cf3e07';
+$curl = curl_init($service_url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$curl_response = curl_exec($curl);
+
+if ($curl_response === false) {
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+    die('error occured during curl exec. Additional info: ' . var_export($info));
+}
+curl_close($curl);
+
+$decoded = json_decode($curl_response,true);
+if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+    die('error occured: ' . $decoded->response->errormessage);
+}
+
+print $decoded;
+echo json_encode($decoded);
+?>
