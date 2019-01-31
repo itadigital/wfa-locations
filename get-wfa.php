eeabf6595a4de67cce541ee30f12bc0c9d48f937
itadigital <csaldanha@itagroup.com>
2019-01-31 14:26:01 -0600

    Update collection id

--- get-wfa.php
+++ get-wfa.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
-$service_url = 'https://api.webflow.com/collections/5c520f9d8077b4ed31a73cd2/items?api_version=1.0.0&access_token=a6bb5ed5a1d1a88a0ef1a37be52f58c12892c489981aff5f8db49d8a37cf3e07';
+$service_url = 'https://api.webflow.com/collections/5c53180a1c2341786cd37b6c/items?api_version=1.0.0&access_token=a6bb5ed5a1d1a88a0ef1a37be52f58c12892c489981aff5f8db49d8a37cf3e07';
 $curl = curl_init($service_url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $curl_response = curl_exec($curl);
